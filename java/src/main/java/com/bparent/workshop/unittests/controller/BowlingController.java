39e2764afc171712842e3e4fea43866f318e7a60
Casou <casou.acc@gmail.com>
2021-01-13 14:39:58 +0100

    Refacto endpoint urlsÃ

--- java/src/main/java/com/bparent/workshop/unittests/controller/BowlingController.java
+++ java/src/main/java/com/bparent/workshop/unittests/controller/BowlingController.java
@@ -6,9 +6,11 @@ import com.bparent.workshop.unittests.util.ScoreFormatter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/game")
 public class BowlingController {
 
     private static final BowlingGame game = new BowlingGame();
@@ -22,7 +24,7 @@ public class BowlingController {
     @PostMapping("/clear")
     public ResponseEntity<String> clearScore() {
         game.clear();
-        return ResponseEntity.ok("Cleared");
+        return ResponseEntity.ok("Cleareddd");
     }
 
 }
