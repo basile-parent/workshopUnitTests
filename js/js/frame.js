ff94b4e6e38e4e1b28ff21056f707f7efbb8fb4f
Casou <casou.acc@gmail.com>
2021-01-18 13:00:59 +0100

    Fix unit tests

--- js/js/frame.js
+++ js/js/frame.js
@@ -1,25 +1,4 @@
-class BowlingGame {
-  constructor() {
-    this.initScores();
-  }
-
-  initScores() {
-    this.frames = [];
-    for (let i = 0; i < 10; i++) {
-      this.frames.push(new Frame());
-    }
-  }
-
-  addShot(shotValue) {
-    this.updateScoreTable();
-  }
-
-  updateScoreTable() {
-    document.getElementById("score-table").innerHTML = this.frames.map((f, index) => f.render(index + 1)).join("");
-  }
-}
-
-class Frame {
+export default class Frame {
   constructor(firstShot = null, secondShot = null, score = null) {
     this.firstShot = firstShot;
     this.secondShot = secondShot;
